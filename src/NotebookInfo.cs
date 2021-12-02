8f4c7968c56c11246993555677625cc089ba8cd9
Saara <jonessaara@gmail.com>
2021-12-02 15:54:32 +0100

    [wip] Notebook tabs

--- src/NotebookInfo.cs
+++ src/NotebookInfo.cs
@@ -6,8 +6,8 @@ public class NotebookInfo : Node2D
 	// Declare member variables here. Examples:
 	private MarginContainer TC;
 	private Label Text;
-	private Color Hover = new Color(1,1,1,1);
-	private Color C = new Color(0,0,0,1);
+	private Color Hover = new Color(1,1,1);
+	private Color C = new Color("#876853");
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
