e2a2cb77aebda0cd3c943241939d5342a6094e8d
Andrew Dobis <andrew.dobis@epfl.ch>
2021-12-08 10:28:09 +0100

    changed file structure a bit

--- src/NotebookInfo.cs
+++ src/NotebookInfo.cs
@@ -1,8 +1,7 @@
 using Godot;
 using System;
 
-public class NotebookInfo : Node2D
-{
+public class NotebookInfo : Node2D {
 	// Declare member variables here. Examples:
 	private MarginContainer TC;
 	private Label Text;
@@ -10,27 +9,20 @@ public class NotebookInfo : Node2D
 	private Color C = new Color("#876853");
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
+	public override void _Ready() {
 		TC = GetNode<MarginContainer>("TextContainer");
 		Text = GetNode<Label>("TextContainer/Text");
 	}
+	
+	public void _UpdateText(string text) {
+		Text.text = text;
+	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
-
-	private void _on_TextContainer_mouse_entered()
-	{
+	private void _on_TextContainer_mouse_entered() {
 		Text.Set("custom_colors/font_color", Hover);
 	}
 	
-
-	private void _on_TextContainer_mouse_exited()
-	{
+	private void _on_TextContainer_mouse_exited() {
 		Text.Set("custom_colors/font_color", C);
 	}
-
 }
---
+++ src/NotebookList.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public class NotebookList : Node2D {
+	[Export]
+	string DBFileName;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready() {
+		
+	}
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
