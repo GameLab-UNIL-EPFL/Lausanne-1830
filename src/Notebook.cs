44047c6a023ffc4ced7d60f440d5e22dbfdf3e1f
Saara <jonessaara@gmail.com>
2021-12-09 13:47:21 +0100

    Sound Test

--- src/Notebook.cs
+++ src/Notebook.cs
@@ -4,13 +4,18 @@ using System;
 public class Notebook : Node2D {
 	
 	private bool hidden = true;
+	private AudioStreamPlayer ASP;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready() {
 		Hide();
+		ASP = GetNode<AudioStreamPlayer>("../NotebookClick");
 	}
 	
 	public void _on_NotebookController_pressed() {
+		if(ASP.Playing == false){
+			ASP.Play();
+		}
 		if(hidden) {
 			Show();
 		} else {
