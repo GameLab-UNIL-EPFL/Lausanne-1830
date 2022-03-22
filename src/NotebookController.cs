b0913d7fe84013b0c16ecd418d13ac9022d2c2c5
Saara <jonessaara@gmail.com>
2022-03-22 15:06:56 +0100

    new clue sprites
    + new icons

--- src/NotebookController.cs
+++ src/NotebookController.cs
@@ -1,25 +1,18 @@
 using Godot;
 using System;
 
-public class NotebookController : Button
+public class NotebookController : TextureButton
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-	private Texture I;
-	private Texture Hover;
 	private AnimationPlayer AnimPlayer;
-	private Button MB;
+	private TextureButton MB;
 	private Notebook NB;
 	private Map M;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Hover = (Texture)GD.Load("res://assets/04_notebook/journal2.png");
-		I = (Texture)GD.Load("res://assets/04_notebook/journal.png");
 		AnimPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-		MB = GetNode<Button>("MapButton");
+		MB = GetNode<TextureButton>("MapButton");
 		NB = GetNode<Notebook>("../Notebook");
 		M = GetNode<Map>("../Notebook/Map");
 		
@@ -31,15 +24,6 @@ public class NotebookController : Button
 //  {
 //      
 //  }
-	private void _on_NotebookController_mouse_entered() {
-	// Replace with function body.
-		Icon = Hover;
-	}
-
-
-	private void _on_NotebookController_mouse_exited() {
-		Icon = I;
-	}
 	
 	private void _on_Player_SlideInNotebookController() {
 		if(AnimPlayer == null) {
