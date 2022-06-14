85be5ee33fb132e12456ea99d98340ad2a9fe4a5
Saara <jonessaara@gmail.com>
2022-06-14 11:28:44 +0200

    arrows and awsd

--- src/Intro.cs
+++ src/Intro.cs
@@ -8,6 +8,8 @@ public class Intro : Node2D {
 	StaticBody2D Door;
 	Label dirTuto;
 	Label interactTuto;
+	Node2D Arrows;
+	Node2D Awsd;
 	
 	//Hide the door if we're no longer in the tutorial
 	public override void _Ready() {
@@ -16,6 +18,8 @@ public class Intro : Node2D {
 		context = GetNode<Context>("/root/Context");
 		dirTuto = GetNode<Label>("Label");
 		interactTuto = GetNode<Label>("Label2");
+		Arrows = GetNode<Node2D>("Label/Arrows");
+		Awsd = GetNode<Node2D>("Label/Awsd");
 		
 		//Check if we're still in the intro or not
 		if(context._GetGameState() != GameStates.INIT) {
@@ -27,5 +31,11 @@ public class Intro : Node2D {
 			dirTuto.Hide();
 			interactTuto.Hide();
 		}
+	}
+	
+	private void _on_Timer_timeout()
+	{
+		Arrows.Visible = !Arrows.Visible;
+		Awsd.Visible = !Awsd.Visible;
 	}
 }
